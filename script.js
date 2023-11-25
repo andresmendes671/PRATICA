664f5047df18e768aacfd63844af8a2de067af0c
andresmendes671 <andresmendes671@outlook.com>
2023-11-25 12:14:48 -0300

    criaçao de arquivo JS

---
+++ script.js
@@ -0,0 +1,62 @@
+const convertButton = document.querySelector(".button-converter")
+
+
+
+
+function convertvalue() {
+
+    const selectConvert = document.querySelector(".select-currency-convert").value
+    const currencyValueToconvert = document.querySelector(".valor-da-moeda-a-convrter")
+    const currencyValueConvert = document.querySelector(".valor-da-moeda-convertida")
+    const inputCurrency = document.querySelector(".input-currency").value
+
+    const dolarToDay = 5.2
+    const euroToDay = 6.2
+    const peosToDay = 0.014
+
+
+
+
+
+    if (selectConvert.value== "dolar"){
+        currencyValueConvert.innerHTML = new Intl.NumberFormat("en-US", {
+
+            style: "currency",
+            currency: "USD"
+
+        }).format(inputCurrency / dolarToDay)
+
+    }
+
+    if ( selectConvert.value == "euro"){
+        currencyValueConvert.innerHTML = new Intl.NumberFormat("de-DE", {
+
+            style: "currency",
+            currency: "EUR"
+
+        }).format(inputCurrency / euroToDay)
+    }
+
+    if  ( selectConvert.value =="peso"){
+        currencyValueConvert.innerHTML = new Intl.NumberFormat("ar-AR", {
+
+            style: "currency",
+            currency: "ARS"
+
+        }).format(inputCurrency / peosToDay)
+
+    }
+
+
+
+
+    currencyValueToconvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+
+        style: "currency",
+        currency: "BRL",
+
+    }).format(inputCurrency)
+}
+
+
+convertButton.addEventListener("click", convertvalue)
